9fd036ec79fcca8a29f7a484edad9342734a4af9
Kashika2008Goel <75621276+Kashika2008Goel@users.noreply.github.com>
2021-03-26 10:30:15 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,6 +5,19 @@ var form, game, player;
 var allPlayers;
 
 var car1, car2, car3, car4, cars;
+var car1Img, car2Img, car3Img, car4Img ;
+
+var trackImg;
+
+function proload(){
+  car1Img = loadImage("car1.png");
+  car2Img = loadImage("car2.png");
+  car3Img = loadImage("car3.png");
+  car4Img = loadImage("car4.png");
+
+  trackImg = loadImage("track.jpeg")
+
+}
 
 
 function setup(){
@@ -19,12 +32,16 @@ function setup(){
 
 function draw(){
 
-  if(playerCount == 2){
+  if(playerCount == 4){
     game.writeState(1);
   }
 
   if(gameState == 1){
     game.play();
+  }
+
+  if(gameState == 2){
+    game.end();
   }
 
 }
